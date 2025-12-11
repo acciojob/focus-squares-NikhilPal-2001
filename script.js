edda26b67d016f60ae28bf2f52265b7396aa5ed6
NIKHIL PAL <nikhilpal9312@gmail.com>
2025-12-11 14:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll(".square");
+
+squares.forEach((sq) => {
+  sq.addEventListener("mouseover", () => {
+    squares.forEach((otherSq) => {
+      if (otherSq !== sq) {
+        otherSq.style.backgroundColor = "#6F4E37"; // Coffee
+      }
+    });
+  });
+
+  sq.addEventListener("mouseout", () => {
+    squares.forEach((otherSq) => {
+      otherSq.style.backgroundColor = "#E6E6FA"; // Lavender
+    });
+  });
+});
